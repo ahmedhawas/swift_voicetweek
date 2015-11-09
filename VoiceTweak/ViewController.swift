046c53c6a927b6b7143879e2f506d4179207ae5d
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2015-11-08 20:28:26 -0500

    Initial Commit

---
+++ VoiceTweak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceTweak
+//
+//  Created by Ahmed Hawas on 2015-11-08.
+//  Copyright © 2015 Ahmed Hawas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
